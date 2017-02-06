32f60148d34b8a481c9a2093939ea2b0119d3418
sah <sah@HMBX1CARBON0323>
2017-02-06 09:02:16 -0500

    added converter to fix empty tooltip issues

---
+++ WpfClient/WpfClient/Converters/NullIfEmptyStringConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfClient.Converters
+{
+    public class NullIfEmptyStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return string.IsNullOrEmpty(value as string) ? null : value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException($"{nameof(NullIfEmptyStringConverter)} can only be used OneWay.");
+        }
+    }
+}
